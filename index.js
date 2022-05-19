1e543840d509a0a7ec5a6edea4a031e2aef7f75b
Rin <call0me0rin0chan@gmail.com>
2022-05-19 08:54:37 +0200

    swiggity swigger, just some small stuff, dw abt it

--- index.js
+++ index.js
@@ -5,7 +5,3 @@ var clickboost = 1;
 var clickText = document.getElementById("clicks");
 var text = document.createTextNode("This just got added");
 text.appendChild(clickText);
-
-<h1 id="clicks"><h1>
-
-<button onclick="Click(clicks, clickboost)"></button>
